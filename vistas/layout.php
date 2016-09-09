0a377e3e2d761358f368570b795f35aedff9302c
valde1992 <fervaldex1992@gmail.com>
2016-09-09 02:31:40 -0300

    Se manejan las rutas (hardcode)

--- vistas/layout.php
+++ vistas/layout.php
@@ -7,8 +7,8 @@
        	<?php endif; ?>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/assets/css/crud.css">
-        <script src="/assets/bower_components/jquery/dist/jquery.min.js"></script>
-        <script src="/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="/CrudValdePhp/assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/CrudValdePhp/assets/css/crud.css">
+        <script src="/CrudValdePhp/assets/bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="/CrudValdePhp/assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
     </head>
