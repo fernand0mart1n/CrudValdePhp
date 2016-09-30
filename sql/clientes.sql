a5ce57378f00ea4b3d50b99fa05dbe49be4e0178
valde1992 <fervaldex1992@gmail.com>
2016-09-30 01:07:34 -0300

    Agregado sql con nuevos cambios en /sql/clientes.sql

--- sql/clientes.sql
+++ sql/clientes.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 14-09-2016 a las 17:07:43
+-- Tiempo de generación: 30-09-2016 a las 00:18:39
 -- Versión del servidor: 5.7.13-0ubuntu0.16.04.2
 -- Versión de PHP: 7.0.8-0ubuntu0.16.04.2
 
@@ -40,7 +40,9 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`id`, `nombre`, `apellido`, `fecha_nac`, `nacionalidad_id`, `activo`) VALUES
-(6, 'Barry', 'Allen', '2016-09-09', 4, 1);
+(26, 'sdadsadasd', 'asdsadsadasdsad', '2016-09-21', 4, 1);
+(26, 'Fernando', 'Valdebenito', '1992-04-07', 2, 1);
+(26, 'Barry', 'Allen', '1990-09-21', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -63,6 +65,68 @@ INSERT INTO `nacionalidades` (`id`, `nacionalidad`) VALUES
 (3, 'Brasil'),
 (4, 'Perú');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `nombre` char(50) NOT NULL,
+  `descripcion` char(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'alta_clientes', 'Clientes. Dar de alta nuevo cliente.'),
+(2, 'editar_clientes', 'Clientes. Modificar un cliente.'),
+(3, 'baja_clientes', 'Clientes. Dar de baja un cliente.');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre_usuario` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre_usuario`, `password`) VALUES
+(1, 'usuario', '*96B0150C50489D818DA193ADB55F29A1E4C91D11'),
+(2, 'administrador', '*EFC258DA67C9F942793CDD8A2050C469256C7192');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_rol`
+--
+
+CREATE TABLE `usuario_rol` (
+  `usuario` int(11) DEFAULT NULL,
+  `rol` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario_rol`
+--
+
+INSERT INTO `usuario_rol` (`usuario`, `rol`) VALUES
+(1, 1),
+(2, 1),
+(2, 2),
+(2, 3);
+
 --
 -- Índices para tablas volcadas
 --
@@ -81,6 +145,25 @@ ALTER TABLE `nacionalidades`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario_rol`
+--
+ALTER TABLE `usuario_rol`
+  ADD KEY `usuario` (`usuario`),
+  ADD KEY `rol` (`rol`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -88,12 +171,33 @@ ALTER TABLE `nacionalidades`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT de la tabla `nacionalidades`
 --
 ALTER TABLE `nacionalidades`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usuario_rol`
+--
+ALTER TABLE `usuario_rol`
+  ADD CONSTRAINT `usuario_rol_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `usuario_rol_ibfk_2` FOREIGN KEY (`rol`) REFERENCES `roles` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
